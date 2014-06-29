eec31d4583616886cf4cdbdc762c4a29572ffa2b
Takao Taira <taira@hanoi.jp>
2014-06-29 20:55:53 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TTairaSandbox::Application.config.secret_key_base = '87b53d36795db317fc93ce07676784577708b7607baedc631de1097c379bea39dad0fc8a365441be28dbf945a57ffd320f402ed4d1b84499955c74499c507ac1'
